ab5ecc21ce53721b75a4bd4264782ed41a51c84d
Heti <heti@cognitivo.in>
2016-04-20 09:46:15 -0400

    corrected a bug of filters

--- app/Http/Controllers/DiscoveryController.php
+++ app/Http/Controllers/DiscoveryController.php
@@ -101,7 +101,7 @@ class DiscoveryController extends Controller
           $parameter_details[$parameter_explode[1]]["type"] = $type;
           $parameter_jasper = '\$P{' . $parameter_explode[1] . '}';
           $query_split = preg_split('/' . $parameter_jasper . '/',$query);
-          if(count($query_split)>1){
+          if(count($query_split)>=1){
             $table_col_name=explode(' ',$query_split[0]);
             $pos = count($table_col_name)-3;
             $parameter_details[$parameter_explode[1]]["tablecol"] = $table_col_name[$pos];
